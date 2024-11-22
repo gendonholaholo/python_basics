a01a774cb0603aba2b903568fcb0760e87fcc6c5
Mas Gendon <67081785+gendonholaholo@users.noreply.github.com>
2024-11-23 01:04:49 +0700

    Menambahkan finally | Exception.py

--- Exception.py
+++ Exception.py
@@ -13,7 +13,7 @@ print("\n\033[4mPenggunaan 2 | Multi except\033[0m")
 try:
     x = int(input("Masukkan angka pertama: "))
     y = int(input("Masukkan angka kedua: "))
-    hasil = x / y
+    hasil = x/y
     print("Hasil pembagian:", hasil)
 except ValueError:
     # Error yang menangkap jika input bukan angka
@@ -35,3 +35,21 @@ except ValueError:
 else:
     # Jika tidak ada exception, blok ini dijalankan
     print("Hasil penjumlahan:", hasil)
+
+
+print("\n\033[4mPenggunaan 4 | Menggunakan finally\033[0m")
+try:
+    angka = int(input("Masukkan angka: "))
+    hasil = 10/angka
+    print(f"Hasil pembagian 10 dengan {angka} adalah {hasil}")
+except ValueError as e:
+    print(f"terjadi Kesalahan: {e}. Input yang dimasukan bukan angka.")
+except ZeroDivisionError as e:
+    print(f"Terjadi kesalahan: {e}. Tidak bisa membagi dengan angka 0")
+except Exception as e:
+    print(f"Terjadi kesalahan yang tidak terduga: {e}")
+else:
+    print("Pembagian berhasil dilakukan.")
+finally:
+    print("Terima kasih telah mencoba program ini!")
+
