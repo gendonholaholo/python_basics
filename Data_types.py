f0516b2250610b86de0d6b1ac9e29446dd9ce136
Mas Gendon <67081785+gendonholaholo@users.noreply.github.com>
2024-11-28 16:28:00 +0700

    Menambahkan data types

---
+++ Data_types.py
@@ -0,0 +1,34 @@
+# String
+_str = str("Lisa") # str tipe data menyimpan text
+
+# Number
+_float = float(.7) # float tipe data menyimpan desimal
+_int = int(7) # int tipe data menyimpan desimal
+_complex = complex(7j) # complex tipe data menyimpan desimal
+
+# Array
+_list = ["lisa", "leo", "gos"] # list menimpan nilai bertipe apa pun berjumlah lebih dari satu dan isinya dapat diubah
+_tuple = ("lisa", "leo", "gos") # tuple menyimpan nilai bertipe apa pun berjumlah lebih dari satu dan isinya tidak dapat diubah
+_range = range(6) # range secara otomatis mengeluarkan nilai dari 0-5
+
+# Dict
+_dict = {"nama" : "Lisa", "age" : 36} # dict menyimpan pasangan key-value, key bersifat unik (tidak boleh sama)
+
+# Set
+_set = {"lisa", "leo", "gos"} # set menyimpan nilai unik tanpa urutan
+_frozenset = frozenset({"gos", "leo", "lisa"}) # frozenset mirip dengan set, tetapi sifatnya tidak dapat diubah
+
+# Logic
+_bool = True # bool hanya menyimpan True dan Fa
+
+# Bytes
+_bytes = b"Hello" # bytes menyimpan urutan byte, sering digunakan untuk data biner
+_bytearray = bytearray(5) # mytearray mirip dengan bytte, tetapi isinya dapat diubah
+
+# Memory View
+_memoryview = memoryview(bytes(5)) # memoryview memberikan tampilan langsung dari objek bytes yang dapat diakses tanpa salinan
+
+# None
+_none = None # None menunjukan bahwa suatu variable tidak memiliki nilai
+
+# Coba jalankan satu-satu menggunakan built-in function print!
