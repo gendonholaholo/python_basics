05c6b776c63827433ac970aa4799d84d032f6338
Mas Gendon <67081785+gendonholaholo@users.noreply.github.com>
2024-11-30 15:30:25 +0700

    menambahkan contoh penggunakan module_3

--- penggunan_modul/main.py
+++ penggunan_modul/main.py
@@ -1,8 +1,10 @@
 from module import perkalian, pembagian
 from module_2.penjumlahan import penjumlahan
 from module_2.pengurangan import pengurangan
+import module_3
 
 print(perkalian(4,5))
 print(pembagian(10,5))
 print(penjumlahan(10,5))
 print(pengurangan(10,5))
+module_3.say_hello("Lisa")
---
+++ penggunan_modul/module_3.py
@@ -0,0 +1,2 @@
+def say_hello(nama):
+    print(f"Hallo {nama}")
